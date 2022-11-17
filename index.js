010d2c7f609f2f621361b7c52b7431484de1749c
meelis65 <90324046+meelis65@users.noreply.github.com>
2022-11-17 20:23:54 +0200

    As a user I want to see a list of notes that I created. [#183637609]

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const yamljs = require('yamljs')
 const swaggerDocument = yamljs.load('./swagger.yaml');
-
+const notes = [{"createdAt": "1999-12-31 00:00:00", "title": "string", "content": "string"}]
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.listen(3000, ()=>console.log('http://localhost:3000/docs'))
+app.get('/notes', (req, res) => {
+    res.send(notes)
+})
+app.listen(3000, () => console.log('http://localhost:3000/docs'))
